1bc22fb24989140c73ee2bbc648127071de7f38d
rohmantr <rohmanahmadtaufiq@gmail.com>
2026-07-17 14:02:45 +0700

    feat(backend): add TS migration classes

---
+++ src/migrations/20260713000001-create-tokens.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import * as fs from "fs";
+import * as path from "path";
+
+export class CreateTokens20260713000001 implements MigrationInterface {
+    name = 'CreateTokens20260713000001';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const sql = fs.readFileSync(path.join(__dirname, "20260713000001-create-tokens.sql"), "utf8");
+        await queryRunner.query(sql);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE IF EXISTS tokens CASCADE;`);
+    }
+}
---
+++ src/migrations/20260713000002-create-risk-assessments.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import * as fs from "fs";
+import * as path from "path";
+
+export class CreateRiskAssessments20260713000002 implements MigrationInterface {
+    name = 'CreateRiskAssessments20260713000002';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const sql = fs.readFileSync(path.join(__dirname, "20260713000002-create-risk-assessments.sql"), "utf8");
+        await queryRunner.query(sql);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE IF EXISTS risk_assessments CASCADE;`);
+    }
+}
